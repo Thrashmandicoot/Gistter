12e961fea7bf190246279bb1d3c527da68871891
Thrashmandicoot <Thrashmandicoot@users.noreply.github.com>
2015-09-07 21:44:47 -0700

    Add route for index page

---
+++ javascript/router.js
@@ -0,0 +1,9 @@
+angular.module('Gistter').config([$routeProvider, function($routeProvider) {
+  $routeProvider
+
+    .when('/', {
+      controller: 'IndexController',
+      templateUrl: 'templates/index/index.html'
+    });
+    
+}]);
