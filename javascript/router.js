ecaa5726c56e75392c78f64c98259fac59d1b426
Thrashmandicoot <Thrashmandicoot@users.noreply.github.com>
2015-09-11 14:23:22 -0700

    Add otherwise route, change routes to tweets & repos

--- javascript/router.js
+++ javascript/router.js
@@ -3,9 +3,17 @@ angular.module('Gistter')
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider
 
-    .when('/', {
+    .when('/tweets', {
       controller: 'TwitterController',
-      templateUrl: 'templates/index/index.html'
+      templateUrl: 'templates/index/tweets.html'
+    })
+    .when('/repos', {
+      controller: 'TwitterController',
+      templateUrl: 'templates/index/repos.html'
+    })
+    .otherwise('/tweets', {
+      controller: 'TwitterController',
+      templateUrl: 'templates/index/tweets.html'
     });
 
 }]);
